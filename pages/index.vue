6f811d60cd0befd6d303c8967f62e01b7a5ac9bc
saimain-primary <saimain.primary@gmail.com>
2024-05-11 22:09:51 +0630

    init

---
+++ pages/index.vue
@@ -0,0 +1,28 @@
+<script setup>
+const { data } = await useFetch('/api/tickers/?limit=10')
+</script>
+
+<template>
+    <h1>Index Page</h1>
+
+    <table border="1 px solid">
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Symbol</th>
+                <th>Price</th>
+                <th>Details</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr v-for="currency in data.data" :key="currency.id">
+                <td>{{ currency.name }}</td>
+                <td>{{ currency.symbol }}</td>
+                <td>{{ currency.price_usd }}</td>
+                <td>
+                    <NuxtLink :to="'/currency/' + currency.id">{{ currency.id }}</NuxtLink>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</template>
